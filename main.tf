3be5177fcc615b5c19609e1d740ab952daf2206f
Semyon Gamburg <bsg050@012.net.il>
2020-03-30 12:57:29 +0300

    updated main.tf

--- main.tf
+++ main.tf
@@ -5,10 +5,26 @@ provider "aws"  {
 resource "aws_instance" "semvm" {
    ami = "ami-0fc20dd1da406780b"
    instance_type = "t2.micro"
+   vpc_security_group_ids = [aws_security_group.instance.id]
+
+   user_data = <<-EOF
+               #!/bin/bash
+               echo "Hello, World!" > index.html
+               nohup busybox httpd -f -p 8080 &
+               EOF
+
 
    tags = {
-     Name = "terraform-sem1"
+     Name = "terraform-sem"
    }
-
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+      from_port = 8080
+      to_port   = 8080
+      protocol  = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+          }
+}
